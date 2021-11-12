d720d26a7b83c9974e542e1dbb5ce784a77fbe57
haruka.yoshida <mindmapping71@gmail.com>
2021-11-12 13:03:58 +0000

    second commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,21 +1,22 @@
 # メソッドを定義
 def fizzbuzz(num)
-    if (num % 3 == 0) && (num % 5 == 0)
-        puts 'FizzBuzz'
-    elsif num % 3 == 0
-        puts 'Fizz'
-    elsif num % 5 == 0
-        puts 'Buzz'
-    else
-        puts num
-    end
+  if (num % 3 == 0) && (num % 5 == 0)
+    puts 'FizzBuzz'
+  elsif num % 3 == 0
+    puts 'Fizz'
+  elsif num % 5 == 0
+    puts 'Buzz'
+  else
+    puts num
+  end
+
 end
 
 num = 1
 num_max = 100
 while num <= num_max
-   puts fizzbuzz(num)
-   num+= 1
+  fizzbuzz(num)
+  num += 1
 end
 
 # 以下は動作確認用の記述です
